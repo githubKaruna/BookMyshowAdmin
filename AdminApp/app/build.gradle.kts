7a5567375f40f1a92c60cf24d803ad119a83a5f7
githubKaruna <karunadarekarkunde@gmail.com>
2024-08-23 04:28:35 +0530

    Internet wifi off screendone

--- AdminApp/app/build.gradle.kts
+++ AdminApp/app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.8")
 
 
 }
---
+++ AdminApp/app/src/main/java/com/neatroots/bookymyshowadmin/presentation/screens/InternetAcreen.kt
@@ -0,0 +1,63 @@
+package com.neatroots.bookymyshowadmin.presentation.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WifiOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.neatroots.bookymyshowadmin.R
+
+@Preview(showSystemUi = true)
+@Composable
+fun InternetScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            imageVector = Icons.Default.WifiOff,
+            contentDescription = "Wi-Fi",
+            modifier = Modifier.size(120.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Checking Internet Connection!",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "This app requires an internet connection. Please check your internet connection...",
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Black.copy(alpha = 0.8f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        GradientButton(text = "Retry", textColor = Color.White) {
+
+        }
+    }
+}
